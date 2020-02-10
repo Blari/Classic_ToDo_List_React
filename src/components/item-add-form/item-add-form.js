591ce7069d8514bf738d40cfec05fe56d37e03e5
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-10 14:22:53 +0300

    final

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,12 +4,35 @@ import './item-add-form.css';
 
 export default class ItemAddForm extends Component{
 
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({label: e.target.value});
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdd(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
     render() {
         return(
-        <div className="item-add-form">
-            <button className="btn btn-outline-secondary"
-            onClick={() => this.props.onItemAdd('Hello World')}>Add Item</button>
-        </div>
-        )
+        <form
+        className="item-add-form d-flex"
+        onSubmit={this.onSubmit}>
+            <input type="text"
+                   className="form-control"
+                   onChange={this.onLabelChange}
+                   placeholder="What needs to be done"
+                   value={this.state.label}
+            />
+            <button className="btn btn-outline-secondary text-nowrap">Add Item</button>
+        </form>
+        );
     }
 }
